fab17c9aadffbb60e49dccc262b9e518b71eee2c
Justin Smestad <justin.smestad@gmail.com>
2015-09-28 18:22:27 -0600

    working slide in panel component

--- app/iterations/route.js
+++ app/iterations/route.js
@@ -1,10 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  displayStory: false,
   model: function(params) {
     return this.store.query('iteration', params);
   },
   actions: {
-
+    openStoryPanel: function(story) {
+      return this.render('story-panel', {
+        outlet: 'story-panel',
+        into: 'application',
+        model: story,
+        controller: 'application'
+      });
+    },
+    closeStoryPanel: function() {
+      return this.disconnectOutlet({
+        outlet: 'story-panel',
+        parentView: 'application'
+      });
+    }
   }
 });
